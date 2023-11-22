5e956fd1b7be38f280761d88f55e5223f121c8dc
Gustavo Pavão <luisgustavopavao@hotmail.com>
2023-11-21 22:02:19 -0300

    netscan

--- projeto/telegram_bot/models.py
+++ projeto/telegram_bot/models.py
@@ -1,10 +1,17 @@
 class Pavaobot:
     def __init__(self, command:str):
         command = command.lstrip().lower()
+        if command.__contains__("_"):
+            
+
+
         if command == "/ipdevides":
             self.ipdevides()
-            
-    def ipdevides():
+
+    def ipdevides(alldevides="yes"):
+        pass
+
+    def hardware(alldevides="yes"):
         pass
         
         
